6e3c456ca22c4e4e9f50443a72258838b39192f7
lifestyleFraB <werbeagentur.marbe@t-online.de>
2026-01-11 07:54:12 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+self.addEventListener('fetch', (event) => {
+  // Dieses Skript fängt Netzwerkanfragen ab, macht hier aber noch nichts Komplexes.
+  // Es ist notwendig, damit der Browser die App als "installierbar" erkennt.
+});
